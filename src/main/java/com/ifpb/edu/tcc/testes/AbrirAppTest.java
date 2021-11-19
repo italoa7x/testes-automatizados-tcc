66194f08067384e7e7bc19ec7d60602607aacebf
Italo Alves <contato.italo1020@gmail.com>
2021-11-19 15:01:01 -0300

    adiciona junit e estrutura inicial para realizar um teste

--- src/main/java/com/ifpb/edu/tcc/testes/AbrirAppTest.java
+++ src/main/java/com/ifpb/edu/tcc/testes/AbrirAppTest.java
@@ -2,17 +2,18 @@ package com.ifpb.edu.tcc.testes;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-
+import org.junit.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AbrirAppTest {
 
-	public static void main(String[] args) throws MalformedURLException {
-		DesiredCapabilities capabilities = new DesiredCapabilities();
+	private AndroidDriver<MobileElement> driver;
+	DesiredCapabilities capabilities = new DesiredCapabilities();
 
+	@Before
+	public void setupData() throws MalformedURLException {
 		capabilities.setCapability("platformName", "Android");
 		capabilities.setCapability("deviceName", "emultador-5544");
 		capabilities.setCapability("automationName", "uiautomator2");
@@ -20,10 +21,33 @@ public class AbrirAppTest {
 		capabilities.setCapability("appActivity", "com.projeto_combate_incendios.MainActivity");
 
 		URL url = new URL("http://127.0.0.1:4723/wd/hub");
-		AndroidDriver<MobileElement> driver = new AndroidDriver<MobileElement>(url, capabilities);
-		
-		
+		driver = new AndroidDriver<MobileElement>(url, capabilities);
 
 	}
 
+
+	@Test
+	public void testaAbrirOAppEfazerLogin() throws InterruptedException {
+		MobileElement campoMatricula = (MobileElement) driver.findElementById("LoginRegistration");
+
+		campoMatricula.click();
+		campoMatricula.sendKeys("12345");
+
+		Thread.sleep(2000);
+
+		MobileElement campoSenha = (MobileElement) driver.findElementById("LoginPassword");
+
+		campoSenha.sendKeys("12345");
+
+		MobileElement botaoEntrar = (MobileElement) driver.findElementById("LoginButton");
+
+		Thread.sleep(2000);
+
+		botaoEntrar.click();
+
+		Thread.sleep(4000);
+
+		driver.quit();
+
+	}
 }
