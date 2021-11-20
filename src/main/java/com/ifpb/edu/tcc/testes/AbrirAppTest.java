1e60bef6ee4e5c02411e4b9401968557b3bf4333
Italo Alves <contato.italo1020@gmail.com>
2021-11-20 16:09:42 -0300

    adicioan teste automatizado para o login com sucesso

--- src/main/java/com/ifpb/edu/tcc/testes/AbrirAppTest.java
+++ src/main/java/com/ifpb/edu/tcc/testes/AbrirAppTest.java
@@ -2,20 +2,34 @@ package com.ifpb.edu.tcc.testes;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import org.junit.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 public class AbrirAppTest {
 
+	// get by id = buscar pelo resource-id
+	// get by accessibility id = content desv
 	private AndroidDriver<MobileElement> driver;
-	DesiredCapabilities capabilities = new DesiredCapabilities();
+	private DesiredCapabilities capabilities = new DesiredCapabilities();
+
+	private final String MATRICULA_USUARIO_TEST = "12345";
+	private final String SENHA_USUARIO_TEST = "abc1234";
 
 	@Before
 	public void setupData() throws MalformedURLException {
+		capabilities.setCapability("autoGrantPermissions", "true");
 		capabilities.setCapability("platformName", "Android");
-		capabilities.setCapability("deviceName", "emultador-5544");
+		capabilities.setCapability("deviceName", "emulator-5554");
 		capabilities.setCapability("automationName", "uiautomator2");
 		capabilities.setCapability("appPackage", "com.projeto_combate_incendios");
 		capabilities.setCapability("appActivity", "com.projeto_combate_incendios.MainActivity");
@@ -25,28 +39,29 @@ public class AbrirAppTest {
 
 	}
 
-
 	@Test
-	public void testaAbrirOAppEfazerLogin() throws InterruptedException {
-		MobileElement campoMatricula = (MobileElement) driver.findElementById("LoginRegistration");
+	public void deveFazerLoginComSucessoInformandoMatriculaEsenhaValida() throws InterruptedException {
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
-		campoMatricula.click();
-		campoMatricula.sendKeys("12345");
+		MobileElement campoMatricula = driver.findElement(MobileBy.AccessibilityId("LoginPageRegistrationValue"));
 
-		Thread.sleep(2000);
+		campoMatricula.sendKeys(MATRICULA_USUARIO_TEST);
 
-		MobileElement campoSenha = (MobileElement) driver.findElementById("LoginPassword");
+		MobileElement campoSenha = driver.findElement(MobileBy.AccessibilityId("LoginPagePasswordValue"));
 
-		campoSenha.sendKeys("12345");
+		campoSenha.sendKeys(SENHA_USUARIO_TEST);
 
-		MobileElement botaoEntrar = (MobileElement) driver.findElementById("LoginButton");
+		MobileElement botaoEntrar = driver.findElement(MobileBy.AccessibilityId("LoginButtonEntry"));
 
 		Thread.sleep(2000);
 
 		botaoEntrar.click();
 
-		Thread.sleep(4000);
+		Thread.sleep(20000);
+	}
 
+	@After
+	public void closeDriver() {
 		driver.quit();
 
 	}
