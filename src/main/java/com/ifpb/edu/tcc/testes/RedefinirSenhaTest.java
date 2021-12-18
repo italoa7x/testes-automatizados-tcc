dd796af721577ee4bf8605797f771c943c85c081
Italo Alves <contato.italo1020@gmail.com>
2021-12-18 08:36:04 -0300

    adiciona suite de testes e testes para redefinir senha

--- src/main/java/com/ifpb/edu/tcc/testes/RedefinirSenhaTest.java
+++ src/main/java/com/ifpb/edu/tcc/testes/RedefinirSenhaTest.java
@@ -1,8 +1,6 @@
 package com.ifpb.edu.tcc.testes;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
-import static org.junit.Assert.assertNull;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -20,7 +18,8 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 
 @FixMethodOrder(MethodSorters.JVM)
-public class LoginTest {
+
+public class RedefinirSenhaTest {
 
 	// get by id = buscar pelo resource-id
 	// get by accessibility id = content desv
@@ -28,7 +27,7 @@ public class LoginTest {
 	private DesiredCapabilities capabilities = new DesiredCapabilities();
 
 	private final String MATRICULA_USUARIO_TEST = "201715020018";
-	private final String SENHA_USUARIO_TEST = "abc1234";
+	private final String SENHA_USUARIO_TEST = "24021998";
 
 	@Before
 	public void setupData() throws MalformedURLException {
@@ -45,40 +44,16 @@ public class LoginTest {
 	}
 
 	@Test
-	public void deveTentarFazerLoginSemInformarUmaMatriculaEsenha1() throws InterruptedException {
-		String mensagemErrorLogin = "Credenciais inválidas!";
+	public void tentaCaptarMensagemDeErroParaRedefinicaoDeSenhaInformandoDuasSenhasDiferentes()
+			throws InterruptedException {
+		String mensagemErro = "As senhas não conferem!";
+		String novaSenha = "abc1234";
+		String confirmacaoSenhaIncorreta = "abc123";
+		// Login
+
 		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
 
-		// inserir credenciais
-		MobileElement campoMatricula = driver.findElement(MobileBy.AccessibilityId("LoginPageRegistrationValue"));
-
-		campoMatricula.sendKeys("");
-
-		MobileElement campoSenha = driver.findElement(MobileBy.AccessibilityId("LoginPagePasswordValue"));
-
-		campoSenha.sendKeys("");
-
-		MobileElement botaoEntrar = driver.findElement(MobileBy.AccessibilityId("LoginButtonEntry"));
-
-		Thread.sleep(2000);
-
-		botaoEntrar.click();
-
-		Thread.sleep(2000);
-
-		MobileElement labelError = driver.findElement(MobileBy.AccessibilityId("LoginErrorLabel"));
-
-		// verifica se a mensagem de erro é igual a mensagem esperada
-		assertEquals(mensagemErrorLogin, labelError.getText());
-
-	}
-
-	@Test
-	public void deveFazerLoginComSucessoInformandoMatriculaEsenhaValida2() throws InterruptedException {
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-
-		String matriculaEsperada = "201715020018";
-		String senhaEsperada = "abc1234";
+		// Realiza o login
 
 		MobileElement campoMatricula = driver.findElement(MobileBy.AccessibilityId("LoginPageRegistrationValue"));
 
@@ -90,14 +65,78 @@ public class LoginTest {
 
 		MobileElement botaoEntrar = driver.findElement(MobileBy.AccessibilityId("LoginButtonEntry"));
 
-		Thread.sleep(2000);
+		Thread.sleep(1500);
 
 		botaoEntrar.click();
 
-		assertEquals(MATRICULA_USUARIO_TEST, matriculaEsperada);
-		assertEquals(SENHA_USUARIO_TEST, senhaEsperada);
+		Thread.sleep(2000);
+
+		// Redireciona o usuário para a tela de redefinir senha
+
+		MobileElement campoNovaSenha = driver.findElement(MobileBy.AccessibilityId("FirstLoginInputNewPassword"));
+
+		campoNovaSenha.sendKeys(novaSenha);
+
+		MobileElement campoConfirmacaoNovaSenha = driver
+				.findElement(MobileBy.AccessibilityId("FirstLoginInputConfirmNewPassword"));
+
+		campoConfirmacaoNovaSenha.sendKeys(confirmacaoSenhaIncorreta);
 
 		Thread.sleep(2000);
+
+		// captar o erro
+		MobileElement labelError = driver.findElement(MobileBy.AccessibilityId("FirstLoginPasswordsInvalids"));
+
+		assertEquals(mensagemErro, labelError.getText());
+
+	}
+
+	@Test
+	public void testaAvalidacaoDoTamanhoDaSenhaEseSaoIguais() throws InterruptedException {
+		String mensagemSenhasValidadas = "As senhas são válidas!";
+		String novaSenha = "abc1234";
+		String confirmacaoSenhaIncorreta = "abc1234";
+		// Login
+
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
+
+		// Realiza o login
+
+		MobileElement campoMatricula = driver.findElement(MobileBy.AccessibilityId("LoginPageRegistrationValue"));
+
+		campoMatricula.sendKeys(MATRICULA_USUARIO_TEST);
+
+		MobileElement campoSenha = driver.findElement(MobileBy.AccessibilityId("LoginPagePasswordValue"));
+
+		campoSenha.sendKeys(SENHA_USUARIO_TEST);
+
+		MobileElement botaoEntrar = driver.findElement(MobileBy.AccessibilityId("LoginButtonEntry"));
+
+		Thread.sleep(1500);
+
+		botaoEntrar.click();
+
+		Thread.sleep(2000);
+
+		// Redireciona o usuário para a tela de redefinir senha
+
+		MobileElement campoNovaSenha = driver.findElement(MobileBy.AccessibilityId("FirstLoginInputNewPassword"));
+
+		campoNovaSenha.sendKeys(novaSenha);
+
+		MobileElement campoConfirmacaoNovaSenha = driver
+				.findElement(MobileBy.AccessibilityId("FirstLoginInputConfirmNewPassword"));
+
+		campoConfirmacaoNovaSenha.sendKeys(confirmacaoSenhaIncorreta);
+
+		Thread.sleep(2000);
+
+		MobileElement botaoAlterar = driver.findElement(MobileBy.AccessibilityId("FirstLoginButtonChangePassword"));
+		// captar a mensagem de senhas validas
+		MobileElement labelSenhasValidas = driver.findElement(MobileBy.AccessibilityId("FirstLoginPasswordsValids"));
+
+		assertEquals(mensagemSenhasValidadas, labelSenhasValidas.getText());
+
 	}
 
 	@After
---
+++ src/main/java/com/ifpb/edu/tcc/testes/SwitchTests.java
@@ -0,0 +1,11 @@
+package com.ifpb.edu.tcc.testes;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses(value = { RedefinirSenhaTest.class, LoginTest.class })
+public class SwitchTests {
+
+}
